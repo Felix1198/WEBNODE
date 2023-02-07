9d219c524352a4e17ce4c0630881f5b363d0bb0a
FELIX <dung.felix@revomedia.asia>
2023-02-07 17:10:14 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(8000,()=> {
+    console.log("Server is running");
+});
